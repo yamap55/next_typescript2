f975523c47d860600f82a74d1cb020659bbed235
yamap55 <tetuya120@hotmail.com>
2019-10-04 14:54:40 +0900

    temp save

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # next_typescript2
+
+https://medium.com/@selvaganesh93/setup-next-js-with-typescript-integration-dece94e43cf5
---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+// const withTypescript = require('@zeit/next-typescript')
+// module.exports = withTypescript()
+
+// next.config.js
+const withTypescript = require('@zeit/next-typescript')
+module.exports = withTypescript({
+  webpack(config, options) {
+    return config
+  }
+})
